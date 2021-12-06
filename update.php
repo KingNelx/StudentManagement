96f05f4b3baa222220dd32157a430f1587665b96
KingNelX.v2 <88817315+KingNelx@users.noreply.github.com>
2021-12-07 02:06:09 +0800

    Update update.php

--- update.php
+++ update.php
@@ -30,8 +30,23 @@
             </div>
 
             <div class="mb-3">
+                <label class="form-label">Year level</label>
+                <input type="text" class="form-control" required placeholder = "Year Level" name = "yearLevel">
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Major</label>
+                <input type="text" class="form-control" required placeholder = "Major" name = "major">
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Avatar</label>
+                <input type="file" class="form-control" required placeholder = "Avatar" name = "avatar">
+            </div>
+
+            <div class="mb-3">
                 <label class="form-label">Email</label>
-                <input type="email" class="form-control" required placeholder = "Email" name = "email">
+                <input type="email" class="form-control" required placeholder = "email" name = "email">
             </div>
 
             <div class="mb-3">
@@ -51,18 +66,20 @@
 <?php
 
     require_once 'connect.php';
-
-    $studentID = $_GET['id'];
+    $studentID =$_GET['id'];
     if(isset($_POST['submit'])){
         $firstName = mysqli_real_escape_string($connect, $_REQUEST['firstName']);
         $lastName = mysqli_real_escape_string($connect, $_REQUEST['lastName']);
         $studentCourse = mysqli_real_escape_string($connect, $_REQUEST['studentCourse']);
+        $yearLevel = mysqli_real_escape_string($connect, $_REQUEST['yearLevel']);
+        $major = mysqli_real_escape_string($connect, $_REQUEST['major']);
+        $avatar = mysqli_real_escape_string($connect, $_REQUEST['avatar']);
         $email = mysqli_real_escape_string($connect, $_REQUEST['email']);
         $password = mysqli_real_escape_string($connect, $_REQUEST['password']);
         $encrypted = md5($password);
         
-        $sql = " UPDATE studentLists SET firstName = '$firstName', lastName = '$lastName' ,
-            studentCourse = '$studentCourse', email = '$email', password = '$encrypted' WHERE studentID = $studentID";
+        $sql = "UPDATE studentLists SET firstName = '$firstName', lastName = '$lastName', studentCourse = '$studentCourse', studentYear = '$yearLevel', studentMajor = '$major', studentAvatar = '$avatar', 
+        email = '$email', password = '$encrypted' WHERE studentID = $studentID";
 
          $result = mysqli_query($connect, $sql);
 
@@ -75,4 +92,3 @@
 
     mysqli_close($connect);
 ?>
-
